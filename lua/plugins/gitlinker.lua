e05655311111d4166a20b6903c5e7cab1c159287
AnveshJarabani <xzput@outlook.com>
2025-10-23 16:11:53 -0500

    🔧 Update plugin versions and fix gitlinker keybinding
    📦 Bump auto-session, snacks.nvim, and tokyonight.nvim
    ⌨️  Change gitlinker open link from <leader>go to <leader>gO

--- lua/plugins/gitlinker.lua
+++ lua/plugins/gitlinker.lua
@@ -5,7 +5,7 @@ return {
     opts = {},
     keys = {
       { "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
-      { "<leader>go", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+      { "<leader>gO", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
     },
   },
 }
